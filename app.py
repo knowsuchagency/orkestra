fa44371de8c3306ce7ebde79c673c53c8c792c30
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2021-05-21 19:19:51 -0700

    examples abound

--- app.py
+++ app.py
@@ -202,24 +202,32 @@ class ExampleStack(cdk.Stack):
         ScheduledValidation(self, "scheduled_validation")
 
 
+class Powertools(cdk.Stack):
+    def __init__(self, scope, id, **kwargs):
+        super().__init__(scope, id, **kwargs)
+        ScheduledValidation(self, "scheduled_validation")
+
+
 app = cdk.App()
 
+Powertools(app, "powertools")
 
-ExampleStack(
-    app,
-    "ExampleOrkestraStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(
-    #     account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
-    # ),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-    # env=core.Environment(account='123456789012', region='us-east-1'),
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-)
+
+# ExampleStack(
+#     app,
+#     "ExampleOrkestraStack",
+#     # If you don't specify 'env', this stack will be environment-agnostic.
+#     # Account/Region-dependent features and context lookups will not work,
+#     # but a single synthesized template can be deployed anywhere.
+#     # Uncomment the next line to specialize this stack for the AWS Account
+#     # and Region that are implied by the current CLI configuration.
+#     # env=cdk.Environment(
+#     #     account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")
+#     # ),
+#     # Uncomment the next line if you know exactly what Account and Region you
+#     # want to deploy the stack to. */
+#     # env=core.Environment(account='123456789012', region='us-east-1'),
+#     # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+# )
 
 app.synth()
--- lambdas/validation.py
+++ lambdas/validation.py
@@ -1,14 +1,18 @@
 import random
-import sys
+import time
+from contextlib import contextmanager
+from dataclasses import dataclass
+from typing import *
+from typing import TypedDict
 
+from aws_lambda_powertools import Logger, Tracer
 from aws_lambda_powertools.utilities.parser import event_parser
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from loguru import logger
 from pydantic import BaseModel
 
 from orkestra import compose
 
-logger.add(sys.stderr, serialize=True)
+SERVICE_NAME = "validation_example"
 
 
 class Person(BaseModel):
@@ -16,9 +20,37 @@ class Person(BaseModel):
     age: int
 
 
+class PersonDict(TypedDict):
+    name: str
+    age: str
+
+
+@dataclass
+class DummyContext:
+    function_name: str = "test"
+    memory_limit_in_mb: int = 128
+    invoked_function_arn: str = (
+        "arn:aws:lambda:eu-west-1:809313241:function:test"
+    )
+    aws_request_id: str = "52fdfc07-2182-154f-163f-5f0f9a621d72"
+
+
+tracer = Tracer(service=SERVICE_NAME)
+
+logger = Logger(service=SERVICE_NAME)
+
+
+@tracer.capture_method
+@contextmanager
+def wait(seconds) -> int:
+    time.sleep(seconds)
+    yield seconds
+
+
 @compose
-def generate_person(event: dict, context):
-    logger.bind(event=event, context=context).info("generating person")
+def generate_person(event: dict, context) -> PersonDict:
+    logger.info("generating person")
+    logger.info("show event as extra", extra={"event": event})
     name = event.get("name", "sam")
     age = event.get("age", random.randrange(100))
     person = Person(
@@ -30,9 +62,30 @@ def generate_person(event: dict, context):
 
 @compose
 @event_parser(model=Person)
-def greet_person(person: Person, context: LambdaContext):
-    logger.bind(person=person.dict(), context=context).info("greeting person")
+def greet_person(person: Person, context: LambdaContext) -> str:
+    logger.info(person.dict())
     return f"hello {person.name}"
 
 
-generate_person >> greet_person
+@compose
+@tracer.capture_lambda_handler
+@event_parser(model=Person)
+def dismiss_person(person: Person, context: LambdaContext) -> int:
+    logger.info({"person": person, "person_dict": person.dict()})
+    seconds = 2
+    tracer.put_annotation(key="waiting_for", value=seconds)
+    with wait(seconds):
+        tracer.put_annotation(key="waited", value=seconds)
+    return seconds
+
+
+@compose
+@tracer.capture_lambda_handler
+@logger.inject_lambda_context(log_event=True)
+def generate_numbers(event: list, context: LambdaContext) -> List[int]:
+    logger.info("collating", extra={"extra": {"event": event}})
+    r = random.Random("".join(map(str, event)))
+    return [r.randrange(100) for _ in range(1, r.randrange(1, 100))]
+
+
+generate_person >> (greet_person, dismiss_person) >> generate_numbers
