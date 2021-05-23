a62f3f5a2dfa1cc058178aaa3fcf8913dd1ffbd7
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2021-05-22 17:28:00 -0700

    add interface tests and pytest markers

---
+++ tests/test_interfaces.py
@@ -0,0 +1,17 @@
+import pytest
+
+from orkestra import compose
+from orkestra.interfaces import AdjacencyList, Composable, Nextable
+
+
+@pytest.mark.interfaces
+class TestInterfaces:
+    @compose
+    def handler(event, context):
+        ...
+
+    def test_composable(self):
+        assert isinstance(self.handler, Composable)
+
+    def test_adjacency_list(self):
+        assert isinstance(self.handler, AdjacencyList)
--- tests/test_synthesis.py
+++ tests/test_synthesis.py
@@ -1,6 +1,9 @@
+import pytest
+
 from app import synth
 
 
+@pytest.mark.slow
 def test_synthesis():
     """assert that all the stacks synthesize."""
     synth()
