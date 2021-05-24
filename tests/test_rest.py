7f097fbcef77c1c2433c9c1f882e742d15d92a56
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2021-05-24 04:18:40 -0700

    add missing test fixture for rest example

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,6 +1,22 @@
+from dataclasses import dataclass
+
 import pytest
 
 from examples.rest import input_order, process_order
+
+
+@pytest.fixture
+def context():
+    @dataclass
+    class LambdaContext:
+        function_name: str = "test"
+        memory_limit_in_mb: int = 128
+        invoked_function_arn: str = (
+            "arn:aws:lambda:eu-west-1:809313241:function:test"
+        )
+        aws_request_id: str = "52fdfc07-2182-154f-163f-5f0f9a621d72"
+
+    return LambdaContext()
 
 
 @pytest.fixture
@@ -8,8 +24,8 @@ def order():
     return {"id": "foo", "item": "hat"}
 
 
-def test_input_order(order):
-    result = input_order(order, None)
+def test_input_order(order, context):
+    result = input_order(order, context)
     assert result["id"] == order["id"]
 
 
@@ -18,5 +34,5 @@ def test_composition():
 
 
 @pytest.mark.slow
-def test_process_order(order):
-    assert process_order(order, None) == order
+def test_process_order(order, context):
+    assert process_order(order, context) == order
