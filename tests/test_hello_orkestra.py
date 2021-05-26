4974ca397258e5bfc722ed1d3235cb1ddc2cf030
Stephan Fitzpatrick <knowsuchagency@gmail.com>
2021-05-26 15:52:11 -0700

    improve test structure

--- tests/test_hello_orkestra.py
+++ tests/test_hello_orkestra.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 import pytest
 
 from examples.hello_orkestra import (
@@ -17,70 +15,51 @@ from examples.hello_orkestra import (
 )
 
 
-@pytest.fixture
-def context():
-    @dataclass
-    class LambdaContext:
-        function_name: str = "test"
-        memory_limit_in_mb: int = 128
-        invoked_function_arn: str = (
-            "arn:aws:lambda:eu-west-1:809313241:function:test"
-        )
-        aws_request_id: str = "52fdfc07-2182-154f-163f-5f0f9a621d72"
-
-    return LambdaContext()
-
-
-@pytest.fixture
-def item():
-    return Item.random().dict()
-
-
 class TestMethods:
     @staticmethod
-    def test_generate_item(item, context):
-        generated = generate_item(item, context)
+    def test_generate_item(generic_event, generic_context):
+        generated = generate_item(generic_event, generic_context)
         assert Item(**generated)
 
     @staticmethod
-    def test_add_price(item, context):
-        result = add_price(item, context)
+    def test_add_price(generic_event, generic_context):
+        result = add_price(generic_event, generic_context)
         assert result["price"]
 
     @staticmethod
-    def test_copy_item(item, context):
-        result = copy_item(item, context)
-        assert all(i == item for i in result)
+    def test_copy_item(generic_event, generic_context):
+        result = copy_item(generic_event, generic_context)
+        assert all(i == generic_event for i in result)
 
     @staticmethod
-    def test_double_price(item, context):
-        item["price"] = 1
-        result = double_price(item, context)
-        assert result["price"] == item["price"] * 2
+    def test_double_price(generic_event, generic_context):
+        generic_event["price"] = 1
+        result = double_price(generic_event, generic_context)
+        assert result["price"] == generic_event["price"] * 2
 
     @staticmethod
-    def test_assert_false(item, context):
+    def test_assert_false(generic_event, generic_context):
         with pytest.raises(AssertionError):
-            assert_false(item, context)
+            assert_false(generic_event, generic_context)
 
     @staticmethod
-    def test_do_nothing(item, context):
-        assert do_nothing(item, context) is None
+    def test_do_nothing(generic_event, generic_context):
+        assert do_nothing(generic_event, generic_context) is None
 
     @staticmethod
-    def test_say_hello(item, context):
-        assert say_hello(item, context)
+    def test_say_hello(generic_event, generic_context):
+        assert say_hello(generic_event, generic_context)
 
     @staticmethod
-    def test_goodbye(item, context):
-        assert say_goodbye(item, context)
+    def test_goodbye(generic_event, generic_context):
+        assert say_goodbye(generic_event, generic_context)
 
     @staticmethod
-    def test_random_int(item, context):
-        result = random_int(item, context)
+    def test_random_int(generic_event, generic_context):
+        result = random_int(generic_event, generic_context)
         assert isinstance(result, int)
 
     @staticmethod
-    def test_random_float(item, context):
-        result = random_float(item, context)
+    def test_random_float(generic_event, generic_context):
+        result = random_float(generic_event, generic_context)
         assert isinstance(result, float)
